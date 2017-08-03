4e74ddaad909ba03cfda32ac6acb2daaf0479cf2
Sergei Rybakov <srybakov@AGLC02NV165G3QC.local>
2017-08-03 16:28:30 -0400

    some clean up of first level navigation

--- header.js
+++ header.js
@@ -1,6 +1,23 @@
+/*Importing Roboto*/
+var css = '@import url(https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin,latin-ext,cyrillic,cyrillic-ext,greek-ext,greek,vietnamese);\
+           @import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,400,400italic,700,700italic&subset=latin,latin-ext,cyrillic-ext,cyrillic,greek-ext,greek,vietnamese);\
+           @import url(https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700&subset=latin,latin-ext,greek-ext,greek,vietnamese,cyrillic,cyrillic-ext);',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+style.type = 'text/css';
+if (style.styleSheet){
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+/* End of Importing Roboto*/
+
+
 var firstMenuItems = [{
     name: 'ADM',
-    triangleColor: '#0094CD',
+    triangleColor: '#0094CD', // color has to be in css
     secondMenu: {
         color: '#0094CD',
         items: [{
@@ -73,11 +90,15 @@ function initialComponent() {
     var html = '';
     firstMenuItems.forEach(function(item) {
         var itemName = '\'' + item.name + '\'';
-        html += '<div><div onclick="header.onFirstMenuItemClick(' + itemName + ')">' + item.name + '</div><div class="base-menu-triangle"><div class="triangle-up triangle-up-first-menu"></div></div></div>';
+        var itemId =  'adm_' + item.name; // need to define better ID's
+        html += '<div class="adm_tab" id="'+itemId+'" data-selected="0" data-hassubmenu="" onclick="header.onFirstMenuItemClick(' + itemName + ')"><div class="adm_tab_label">' + item.name + '</div><div class="base-menu-triangle"><div class="triangle-up triangle-up-first-menu"></div></div></div>';
     });
     $('.header-menu').append(html);
+
+    /* Approptiate triangles should be displayed thought css*/
     hideAllFirstMenuTriangle('.triangle-up-first-menu');
     $($.find('.triangle-up-first-menu')[0]).css({'display': 'block'});
+    //$( '#adm_ADM' ).attr( 'data-selected','--' );
 }
 
 function onFirstMenuItemClick(name) {
@@ -91,7 +112,7 @@ function onFirstMenuItemClick(name) {
     firstMenuItems[index].secondMenu.items.forEach(function(item, index) {
         var itemName = '\'' + item.name + '\'';
         var itemIndex = '\'' + index + '\'';
-        html += '<div><div onclick="secondMenu.onSecondMenuItemClick(' + itemName + ', ' + index + ')">' + item.name + '</div><div class="second-menu-triangle"><div class="triangle-up triangle-up-second-menu"></div></div></div>';
+        html += '<div class="adm_tab"><div onclick="secondMenu.onSecondMenuItemClick(' + itemName + ', ' + index + ')">' + item.name + '</div><div class="second-menu-triangle"><div class="triangle-up triangle-up-second-menu"></div></div></div>';
     });
     $('.second-menu').html('');
     $('.second-menu').append(html);
